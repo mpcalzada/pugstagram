6291bf41ece6436069051a08ddcfaec90b295f54
Marco Calzada <mccalzada@outlook.com>
2021-03-07 23:16:47 -0600

    Adding cards and comments into the TimeLine

---
+++ src/components/Card.svelte
@@ -0,0 +1,37 @@
+<script>
+import Comments from "./Comments";
+
+</script>
+<div class="Card">
+    <div class="Card-container">
+        <div class="Card-user">
+            <img src="https://arepa.s3.amazonaws.com/elmo002.jpg" alt="">
+            <h2>
+                Elmo.jpg
+                <span>Ciudad de México</span>
+            </h2>
+        </div>
+        <div class="Card-settings">
+            <i class="fas fa-ellipsis-h"/>
+        </div>
+        <div class="Card-photo">
+            <figure>
+                <img src="https://arepa.s3.amazonaws.com/elmo002.jpg" alt="">
+            </figure>
+        </div>
+        <div class="Card-icons">
+            <div class="Card-icons-first">
+                <i class="fas fa-heart" />
+                <i class="fas fa-paper-plane" />
+            </div>
+            <div class="Card-icons-second">
+                <i class="fas fa-bookmark" />
+            </div>
+        </div>
+        <div class="Card-description">
+            <h3>Elmo.pug</h3>
+            <span>Hola!</span>
+        </div>
+        <Comments />
+    </div>
+</div>
---
+++ src/components/Comments.svelte
@@ -0,0 +1,22 @@
+<script>
+import { } from "node:os";
+import App from "../containers/App.svelte";
+
+</script>
+<div class="Comments">
+    <div class="Comments-container">
+        <div class="Comments-users">
+            <h3>Crispeta</h3>
+            <span>Hola Elmo</span>
+        </div>
+        <div class="Comments-add">
+            <form>
+                <input type="text"
+                    class="Comments-input"
+                    placeholder="Agregar comentario..."
+                    id="text" />
+                <button type="submit">Post</button>
+            </form>
+        </div>
+    </div>
+</div>
