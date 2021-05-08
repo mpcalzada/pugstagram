bdc2526f4a56d00ec934a84e6dfe28a27e17c9d2
Marco Calzada <mccalzada@outlook.com>
2021-05-08 13:33:21 -0500

    Adding Footer to the project.

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -2,6 +2,15 @@
   const actualYear = new Date().getFullYear();
 </script>
 
+<style>
+  .Footer {
+    font-size: 11px;
+    letter-spacing: 1px;
+    font-weight: normal;
+    color: #c7c7c7;
+  }
+</style>
+
 <div class="Footer">
   <div class="Footer-container">
     <div class="Footer-copy">
